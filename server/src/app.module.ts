298d8b7c1a7e4ee824e7f5a2d296c174a2a41769
ihor_lavrov <ihor.lavrov.2018@gmail.com>
2021-04-19 22:37:22 +0300

    base user table

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
